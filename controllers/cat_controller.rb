599e06da8dfe3ce38e753fa5ac7707c99da41e37
code-for-coffee <jtraverjr@gmail.com>
2016-11-14 11:31:44 -0600

    ActiveRecord and Rake fucking rock

--- controllers/cat_controller.rb
+++ controllers/cat_controller.rb
@@ -3,6 +3,11 @@ class CatsController < Sinatra::Base
   require 'bundler'
   Bundler.require
 
+  ActiveRecord::Base.establish_connection(
+      :adapter => 'mysql2',
+      :database => 'cats_rescue'
+  )
+
   get '/' do
     {:message => 'it is working'}.to_json
   end
