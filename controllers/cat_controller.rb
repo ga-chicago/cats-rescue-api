366287de78d824e905b62d2100c3c40ade5be5d8
code-for-coffee <jtraverjr@gmail.com>
2016-11-14 11:40:20 -0600

    final version

--- controllers/cat_controller.rb
+++ controllers/cat_controller.rb
@@ -9,7 +9,37 @@ class CatsController < Sinatra::Base
   )
 
   get '/' do
-    {:message => 'it is working'}.to_json
+    @model = Cat.all
+    @model.to_json
+  end
+
+  get '/:id' do
+    @model = Cat.find(params[:id])
+    @model.to_json
+  end
+
+  post '/' do
+    @model = Cat.new
+    @model.name = params[:name]
+    @model.note = params[:note]
+    @model.image = params[:image]
+    @model.save
+    @model.to_json
+  end
+
+  patch '/:id' do
+    @model = Cat.find(params[:id])
+    @model.name = params[:name]
+    @model.note = params[:note]
+    @model.image = params[:image]
+    @model.save
+    @model.to_json
+  end
+
+  delete '/:id' do
+    @model = Cat.find(params[:id])
+    @model.destroy
+    {:message => 'Your cat with an id of ' + params[:id] + ' was adopted :)'}.to_json
   end
 
 end
